d54f727fe1a8728284936813d251e90f0bb5b3d5
nitrog42 <nitrog42@gmail.com>
2019-01-30 10:11:27 +0100

    Added a static call for example

--- app/src/main/java/fr/openium/examplepam/rest/NewsApi.java
+++ app/src/main/java/fr/openium/examplepam/rest/NewsApi.java
@@ -6,6 +6,23 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface NewsApi {
+
+    /**
+     * Call a webservice with dynamic path
+     * Exemple :
+     * "https://api.myjson.com/bins/31245"
+     * base url : https://api.myjson.com/
+     * path : bins/{id}
+     * where id = 31245
+     * So the method will be called using getNews("31235");
+     */
+
     @GET("bins/{id}")
     Call<News> getNews(@Path("id") String id);
+
+    /**
+     * Call a webservice with a static path
+     */
+    @GET("https://custom.server.com/file.json")
+    Call<String> getFile();
 }
